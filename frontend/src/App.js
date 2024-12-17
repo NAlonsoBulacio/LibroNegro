2af0f09264f3dd9905bf56e7a348cb8d8245041a
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2024-12-17 17:57:06 -0300

    cleaning

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import { Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage/LandingPage";
-import LandingPageBundle from './pages/LandingPageBundle/LandingPageBundle';
 function App() {
   return (
     <div className="App font-sans-200">
       <Route exact path="/" component={LandingPage} />
-      <Route exact path="/bundle" component={LandingPageBundle} />
     </div>
   );
 }
--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -1,11 +1,5 @@
 import best_seller from "./images/best_seller.png";
-import product from "./images/product.png";
-import product2 from "./images/product2.png";
-import product3 from "./images/product3.png";
-import usa from "./images/usa.png";
 import logo from "./images/logo.png";
-import life from "./images/life.png";
-import logo_blanco from "./images/logo-blanco.png";
 import paymeth from "./images/paymethods.png";
 import person from "./images/person.svg";
 import email from "./images/email.svg";
@@ -30,17 +24,11 @@ import testimonio10 from "./testimonios/10.jpg";
 import testimonio11 from "./testimonios/11.jpg";
 export {
   best_seller,
-  product,
-  product2,
-  product3,
   logo,
   paymeth,
-  life,
-  logo_blanco,
   person,
   phone,
   email,
-  usa,
   libro_negro,
   yes,
   matias,
